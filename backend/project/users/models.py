19fbeeefacdb37851f9767ad264f50140fd4d6d3
Ephantus2 <ephantusmwago6@gmail.com>
2026-09-03 17:02:02 +0300

    initial commit

---
+++ backend/project/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        LECTURER = "LECTURER", "Lecturer"
+        ADMIN = "ADMIN", "Admin"
+        
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
